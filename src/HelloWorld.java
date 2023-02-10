5a72b1a24084292d4116cdccb96e2701650ad006
Baiba Plavina <BPlavina@gmail.com>
2023-02-10 19:17:50 +0200

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ import java.sql.SQLOutput;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Baiba!");
+        System.out.println("Hello, second time!");
     }
 }
