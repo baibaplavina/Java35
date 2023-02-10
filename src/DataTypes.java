a76ffdc409377582c3a476ee8d0c43b16ee08cf2
Baiba Plavina <BPlavina@gmail.com>
2023-02-10 21:33:38 +0200

    Variables and data types

---
+++ src/DataTypes.java
@@ -0,0 +1,38 @@
+import java.sql.SQLOutput;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //Integer Data type
+        byte num1 = 100;
+        short num2 = 1000;
+        int num3 = 3425354;
+        long num4 = 413232345L;
+
+        //Floating poing data type
+        float num5 = 12312.2423f;
+        double num6 = 2424234.234455643d;
+
+        //Boolean data type
+        boolean redLightSwitch = false;
+        boolean blackLightSwitch = true;
+
+        //Character data type
+        char char1 = '%';
+        char char2 = 'y';
+        char char3 = '0';
+        char char4 = '\n';
+
+        //Sting
+        String text1 = "This is very good text";
+
+        // Print out variable values
+        System.out.println(num1);
+        System.out.println(num6);
+        System.out.println(redLightSwitch);
+        System.out.println(blackLightSwitch);
+        System.out.println(char4);
+        System.out.println(char2);
+        System.out.println(text1);
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,12 @@
-import java.sql.SQLOutput;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Baiba!");
-        System.out.println("Hello, second time!");
+        System.out.println("Hello, She Goes Tech!");
+        // This is single- line comment
+        /*
+        This is Multi-line
+        comment
+         */
+
     }
 }
